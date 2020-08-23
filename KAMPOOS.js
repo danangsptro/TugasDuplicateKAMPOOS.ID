66038ca1ef02683ab5c0fcbf2d84fdd9d9e4230a
danangsptro <danangsaputro300@gmail.com>
2020-08-23 22:07:05 +0700

    tugas

---
+++ KAMPOOS.js
@@ -0,0 +1,130 @@
+// KAMPOOS.ID
+// sidebar
+$(document).ready(function(){
+    $("#j1").on({
+        mouseenter: function (){
+            $(this).css("color","blue")
+        },
+        mouseleave: function (){
+            $(this).css("color","gray")
+        }
+    })
+})
+$(document).ready(function(){
+    $(".sd").on({
+        mouseenter: function(){
+            $(this).css("color","white")
+        },
+        mouseleave: function(){
+            $(this).css("color","black")
+        }
+    })
+})
+// content 2
+$(document).ready(function(){
+    $(".vardam").on({
+        mouseenter: function(){
+            $(this).css("color","rgb(1, 1, 177)")
+        },
+        mouseleave:function(){
+            $(this).css("color","black")
+        }
+    })
+})
+$(document).ready(function(){
+    $("#card").hover(function(){
+        $(this).addClass("transisi");
+    }, function(){
+        $(this).removeClass("transisi");
+    });
+});
+$(document).ready(function(){
+    $("#card1").hover(function(){
+        $(this).addClass("transisi");
+    }, function(){
+        $(this).removeClass("transisi");
+    });
+});
+$(document).ready(function(){
+    $("#card2").hover(function(){
+        $(this).addClass("transisi");
+    }, function(){
+        $(this).removeClass("transisi");
+    });
+});
+// content3
+$(document).ready(function(){
+    $(".ct33").on({
+        mouseenter : function(){
+            $(this).css("color","rgb(1, 1, 177)")
+        },
+        mouseleave : function(){
+            $(this).css("color","black")
+        }
+    })
+})
+// content4
+$(document).ready(function(){
+    $(".ctt4").on({
+        mouseenter : function(){
+            $(this).css("color","rgb(1, 1, 177)")
+        },
+        mouseleave : function(){
+            $(this).css("color","black")
+        }
+    })
+})
+
+// NEWS
+// sidebar news
+$(document).ready(function(){
+    $("#news").on({
+        mouseenter: function(){
+            $(this).css("color","darkblue")
+        },
+        mouseleave: function(){
+            $(this).css("color","gray")
+        }
+    })
+})
+// NAVBAR
+$(document).ready(function(){
+    $(".cttj1").on ({
+        mouseenter: function (){
+            $(this).css("color","rgb(1, 1, 177)")
+        },
+        mouseleave: function (){
+            $(this).css("color","black")
+        }
+    })
+})
+// ACADEMY
+// Sidebar
+$(document).ready(function(){
+    $(".acadmy").hover(function(){
+        $(this).addClass("transisi");
+    }, function(){
+        $(this).removeClass("transisi");
+    });
+});
+$(document).ready(function(){
+    $("#academy").on({
+        mouseenter: function(){
+            $(this).css("color","darkblue")
+        },
+        mouseleave: function(){
+            $(this).css("color","gray")
+        }
+    })
+})
+// navbar
+$(document).ready(function(){
+    $(".sss").on ({
+        mouseenter: function (){
+            $(this).css("color","rgb(1, 1, 177)")
+        },
+        mouseleave: function (){
+            $(this).css("color","black")
+        }
+    })
+})
